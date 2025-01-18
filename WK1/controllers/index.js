fe81be5573d43813fcc2c1ba42bc878a87e27dec
jthoeun <johnny.thoeun@gmail.com>
2025-01-17 20:52:50 -0700

    Wk2 first commit

--- WK1/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const awesomeFunction = (req, res, next) => {
-    res.json('Aubrey Thoeun');
-};
-
-const returnAnotherPerson = (req, res, next) => {
-    res.json('Super awesome person');
-};
-
-module.exports = {awesomeFunction, returnAnotherPerson};
