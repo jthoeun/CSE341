16130bd9879c45edeefce8feeb7869ecb3a67c82
jthoeun <johnny.thoeun@gmail.com>
2025-01-22 21:36:05 -0700

    W3 Personal Assignment inital Commit

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -45,4 +45,65 @@ const getSingle = async (req, res, next) => {
   }
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  try {
+    const userId = req.params.id.trim();
+
+    if (!ObjectId.isValid(userId)) {
+      return res.status(400).json({ error: 'Invalid ID format' });
+    }
+
+    const objectId = new ObjectId(userId);
+    const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: objectId });
+
+    if (response.deletedCount > 0) {
+      console.log('Contact deleted successfully');
+      res.status(204).send(); // Successfully deleted
+    } else {
+      res.status(404).json({ error: 'Contact not found' }); // No document matched the provided ID
+    }
+  } catch (error) {
+    console.error('Error in deleteContact:', error);
+    res.status(500).json({ error: 'An error occurred while deleting the contact' });
+  }
+};
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  {rules: { "react/react-in-jsx-scope": "off"}},
+  {env: { "browser": true, "es2021": true, "jest": true}},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
