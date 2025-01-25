d4e508e1e66219268dad50809c1cb5e1e567eab9
jthoeun <johnny.thoeun@gmail.com>
2025-01-24 22:22:36 -0700

    W3- changes to app.js

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongodb = require('./db/connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
+  .use(cors())
   .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+  .use('/', require('./routes')); 
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
