10ee5a0c2be99cbe6ae5a8178593a169eae12b16
jthoeun <johnny.thoeun@gmail.com>
2025-01-24 22:27:53 -0700

    W3 Reverting changes to app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const mongodb = require('./db/connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
-  .use(cors())
   .use(bodyParser.json())
-  .use('/', require('./routes')); 
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
